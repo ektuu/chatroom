78437b9180f976fe79d339e7f820dcb5caeeadec
Ekta Siwach <ektasiwach5304@gmail.com>
2024-07-15 22:52:18 +0530

    Myntra Chatroom project

--- src/AppConstants.ts
+++ src/AppConstants.ts
@@ -1,7 +1,7 @@
 // Head over to the Credentials section in your app on CometChat Dashboard and where you can find your unique App ID, Region and Auth Key. 😊
 
 export const AppConstants = {
-  APP_ID: "APP_ID",
-  REGION: "REGION",
-  AUTH_KEY: "AUTH_KEY",
+  APP_ID: "26101884ff2582ac",
+  REGION: "IN",
+  AUTH_KEY: "7e67fde5429e1df1384df55e3bd60a190d16994f",
 };
